bb1b9e79d6c3d41d8ff12e14e0aeed4e96c156a0
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2021-11-24 09:57:54 -0500

    update eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arrayOne, arrayTwo) {
+
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let element = 0; element < arrayOne.length; element++) {
+
+    /*Recursive function: calls itself until it doesnt -- nested array*/
+    if (Array.isArray(arrayOne[element]) && Array.isArray(arrayTwo[element])) {
+      if (eqArrays(arrayOne[element],arrayTwo[element]) === false) {
+        return false;
+      }
+
+
+    } else if (arrayOne[element] !== arrayTwo[element]) {
+      return false;
+    }
+  }
+  return true;
+  
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
